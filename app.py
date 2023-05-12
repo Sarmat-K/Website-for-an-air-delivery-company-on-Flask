7071eb64152ef9030e0c6996f154b3e41f105135
Sarmat-K <sarmat.karaf.0@gmail.com>
2023-05-12 23:30:24 +0300

    upd

--- app.py
+++ app.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/about')
+def about(about):
+    return "<h1 style = 'color: red'> About !!!!!!!!!!!!!! </h1> "
+
+
+
 if __name__ == "__main__":
     app.run(debug = True )
